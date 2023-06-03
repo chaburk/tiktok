73d183dfacb9ab32043dc96ef27f868df5388b62
Chase <chaseburkdoll@gmail.com>
2023-06-03 14:31:46 -0500

    Added VideoFooter and VideoSidebar components

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./Video.css";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 
 function Video() {
   const [playing, setPlaying] = useState(false);
@@ -23,8 +25,8 @@ function Video() {
         ref={videoRef}
         src="https://player.vimeo.com/external/420239207.sd.mp4?s=2b5a6633c37af1a6fb0beb02c06bdc376fdfcce2&profile_id=165&oauth2_token_id=57447761"
       ></video>
-      {/*Video footer*/}
-      {/*Video sidebar*/}
+      <VideoFooter />
+      <VideoSidebar />
     </div>
   );
 }
